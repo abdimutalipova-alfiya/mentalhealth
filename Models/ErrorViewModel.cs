ae11d530f6293b740fcd9c751dd0de6b0c63d682
Alfiya <114033078+abdimutalipova-alfiya@users.noreply.github.com>
2023-12-17 12:53:03 +0100

    initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Mental_Health.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
